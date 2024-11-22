1267920b13856839689c900067e50972f126ec99
DanConstantin <dan.guita@yahoo.com>
2024-11-22 12:05:48 +0200

    ready

---
+++ src/get-locale.js
@@ -0,0 +1,22 @@
+
+import { match } from "@formatjs/intl-localematcher";
+
+import Negotiator from 'negotiator';
+
+export let locales = ['en', 'ro']
+let defaultLocale = 'en'
+
+
+// Get the preferred locale, similar to the above or using a library
+export function getLocale(request) {
+
+    const headers = {};
+    request.headers.forEach((value, key) => {
+        headers[key] = value;
+    });
+    let languages = new Negotiator({ headers }).languages()
+
+
+
+    return match(languages, locales, defaultLocale);
+}
